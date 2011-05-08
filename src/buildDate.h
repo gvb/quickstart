8a4d6d94d9af245875ebc38f20ea32441ed0bcab
Mike Stitt <mike.stitt@cri.us.com>
2011-05-08 19:44:00 -0400

    chmod  -x, add buildDate.h
    Signed-off-by: Mike Stitt <mike.stitt@cri.us.com>

---
+++ src/buildDate.h
@@ -0,0 +1,22 @@
+/**
+ * \file buildDate.h
+ *
+ * Date program was build
+ *
+ * \addtogroup TBD
+ * \{
+ *//*
+ * Copyright (C) 2011 Consolidated Resource Imaging LLC
+ *
+ *       1         2         3         4         5         6         7
+ *3456789012345678901234567890123456789012345678901234567890123456789012345678
+ *
+ */
+
+#ifndef BUILDDATE_H_
+#define BUILDDATE_H_
+
+extern const char buildDate[];
+
+#endif /* BUILDDATE_H_ */
+/** \} */
