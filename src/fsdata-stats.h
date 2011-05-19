46692e41439c2e6926c671d10e2d78df08d50d77
Mike Stitt <mike.stitt@cri.us.com>
2011-05-19 01:55:28 -0400

    Cut a release to Brian.
    Signed-off-by: Mike Stitt <mike.stitt@cri.us.com>

---
+++ src/fsdata-stats.h
@@ -0,0 +1,11 @@
+/* Generated automatically by ./makefsdata httpd-fs/ fsdata.c fsdata-stats.h */
+
+#ifndef _FSDATA_STATS_H_
+#define _FSDATA_STATS_H_
+
+#define FS_ROOT file_style_css
+extern const struct fsdata_file FS_ROOT[];
+
+#define FS_NUMFILES 24
+
+#endif
