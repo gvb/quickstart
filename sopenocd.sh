a31606182cbb189136f6860c542da35b9d358b29
Gerald Van Baren <gerald.vanbaren@cri.us.com>
2013-07-20 09:42:40 -0400

    Use the OpenOCD package from Ubuntu
    The Ubuntu OpenOCD package now has sufficient support.  Use it instead
    of building our own.  This means the config files are under /usr/share
    insteald of /usr/local/share.
    
    Signed-off-by: Gerald Van Baren <gerald.vanbaren@cri.us.com>

--- sopenocd.sh
+++ sopenocd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openocd -f /usr/local/share/openocd/scripts/interface/luminary.cfg -f /usr/local/share/openocd/scripts/target/stellaris.cfg 
+openocd -f /usr/share/openocd/scripts/interface/luminary.cfg -f /usr/share/openocd/scripts/target/stellaris.cfg 
 
